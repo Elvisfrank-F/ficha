f52793c552dff17a2d60832157b47ce89d2f9d27
Elvisfrank-F <gordotesla@gmail.com>
2025-11-02 21:09:16 -0300

    1 - Botão de adicionar cliente adicionado. 2- Função de pesquisa da 'barra de pesquisar' está funcionando. 3 - mudança de desirgn

--- lib/client.dart
+++ lib/client.dart
@@ -2,28 +2,159 @@ import 'package:flutter/material.dart';
 
 
 class Client extends StatefulWidget {
-  const Client({super.key});
+
+  final String name;
+  final double divida;
+  final String endereco;
+
+  const Client({super.key, required  this.name, required this.divida, required this.endereco});
 
   @override
-  State<Client> createState() => _ClientState();
+  State<Client> createState() => _ClientState(name: name, divida: divida, endereco: endereco);
 }
 
 class _ClientState extends State<Client> {
+
+  String name;
+  double divida;
+  String endereco;
+
+   TextEditingController _controllerName = new TextEditingController();
+   TextEditingController _controllerDivida = new TextEditingController();
+   TextEditingController _controllerEndereco = new TextEditingController();
+
+  _ClientState({required  this.name, required this.divida, required this.endereco});
+
+  @override
+  void initState() {
+    // TODO: implement initState
+
+  _controllerDivida.text = divida.toString();
+  _controllerEndereco.text = endereco;
+  _controllerName.text =  name;
+
+    super.initState();
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(
-      child: Card(
+    String srtDivida = divida.toStringAsFixed(2);
+    return Card(
         elevation: 10,
         child: SizedBox(
-          width: 300,
-          height: 300,
+          width: 230,
+          
           child: Column(
             children: [
-              Icon(Icons.supervised_user_circle),
-              Text("Nome da Cliente"),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [ 
+
+                  Icon(Icons.remove_circle_outline, size: 35),
+                  
+                  GestureDetector(
+                     onTap: () => showDialog<String>(
+                      context: context,
+
+                      builder:(context) => AlertDialog(
+                        title: Text("Editar os dados de $name"),
+
+                        content: SizedBox(
+                height: 300,
+                width: 300,
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: _controllerName,
+                      keyboardType: TextInputType.name,
+                      decoration: InputDecoration(
+                        label: Text('Nome'),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)
+                        )
+                      )
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextField(
+                      controller: _controllerDivida,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        label: Text('Valor da dívida'),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)
+                        )
+                      )
+                    ),
+                      SizedBox(
+                      height: 10,
+                    ),
+                    TextField(
+                      controller: _controllerEndereco,
+                      keyboardType: TextInputType.streetAddress,
+                      decoration: InputDecoration(
+                        label: Text('Endereço'),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)
+                        )
+                      )
+                    ),
+                  ],
+                ),
+              ),
+                            
+                          
+                       
+                        
+                        actions: [
+                          TextButton(child: Text("Cancelar"), onPressed: () => Navigator.pop(context, "Cancelar")),
+                          TextButton(child: Text("Aplicar"), onPressed: () {
+
+                            setState(() {
+                                name = _controllerName.text;
+                            divida = double.tryParse(_controllerDivida.text) ?? 0;
+                            endereco = _controllerEndereco.text;
+                            });
+                          
+                             Navigator.pop(context, 'Aplicar');
+                             
+                             }),
+                        ]
+                      ),
+
+                     ),
+                    
+                    child: Container(
+                    height: 30,
+                    width: 30,
+                    decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 2.0
+                      ),
+                      borderRadius: BorderRadius.circular(10)
+                    ),
+                    child: Icon(Icons.edit)),
+                  ),],
+              ),
+             
+              SizedBox(height: 10),
+              Icon(Icons.account_circle, size: 100),
+              SizedBox(height: 20,),
+              Text(name, style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500)),
+               Text(endereco, style: TextStyle(fontSize: 15, fontWeight: FontWeight.w100)),
+              SizedBox(height: 10),
+              Text("Dívida", style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
+              Text("R\$$srtDivida", style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold))
+
+
+              
+
             ],
           )),
-      ),
-    );
+      );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'client.dart';
+import 'package:logger/logger.dart';
 
 void main(){
   runApp(MyApp());
@@ -11,6 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+  
       home: HomePage()
     );
   }
@@ -24,9 +27,177 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+
+
+   TextEditingController _controllerName = new TextEditingController();
+            TextEditingController _controllerDivida = new TextEditingController();
+            TextEditingController _controllerEndereco = new TextEditingController();
+
+  List<Map<String, dynamic>> listClient = [];
+  List<Map<String, dynamic>> listReClient = [];
+   
+   
+  
+
+  void CreateClient(String name, double divida, String endereco) {
+
+    Client cliente = new Client(divida: divida, name: name, endereco: endereco);
+
+    var logger = Logger();
+
+
+ setState(() {
+   listClient.add({
+    'name': name,
+    'divida' : divida,
+    'endereco' : endereco,
+   }
+   );
+   listReClient = List.from(listClient);
+ });
+
+ _controllerDivida.clear();
+ _controllerEndereco.clear();
+ _controllerName.clear();
+    
+
+   
+  }
+
+  void _search(String a){
+
+    setState(() {
+
+      if(a.isEmpty){
+ listClient = List.from(listReClient);
+ return;
+}
+
+if(listReClient.isEmpty){
+   listReClient = List.from(listClient);
+}
+      
+    
+final resultado = listReClient.where((client){
+
+  final name = client['name'].toString().toLowerCase();
+  return name.contains(a.toLowerCase());
+}).toList();
+
+listClient = resultado;
+
+  });
+
+
+    
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 29, 149, 185),
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 29, 149, 185),
+        centerTitle: true,
+        title: Text("Ficha", style: TextStyle(color: Colors.white, )),
+        leading: Icon(Icons.menu, color: Colors.white),
+        
+        actions: [
+        
+
+        
+          
+         ElevatedButton.icon(
+          style: ButtonStyle(
+            backgroundColor: WidgetStateProperty.all(Colors.transparent),
+            iconColor: WidgetStateProperty.all(Colors.black),
+            ),
+          
+          onPressed: () { 
+            
+           
+          
+            
+            showDialog<String>(
+            
+            context: context,
+            builder: (BuildContext context) => AlertDialog(
+              title: const Text("Adicionar cliente", textAlign: TextAlign.center,
+              style: TextStyle(fontWeight: FontWeight.w400)),
+              
+              content:  SizedBox(
+                height: 300,
+                width: 300,
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: _controllerName,
+                      keyboardType: TextInputType.name,
+                      decoration: InputDecoration(
+                        label: Text('Nome'),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)
+                        )
+                      )
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextField(
+                      controller: _controllerDivida,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        label: Text('Valor da dívida'),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)
+                        )
+                      )
+                    ),
+                      SizedBox(
+                      height: 10,
+                    ),
+                    TextField(
+                      controller: _controllerEndereco,
+                      keyboardType: TextInputType.streetAddress,
+                      decoration: InputDecoration(
+                        label: Text('Endereço'),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)
+                        )
+                      )
+                    ),
+                  ],
+                ),
+              ),
+              actions: <Widget>[
+                TextButton(child: const Text("Cancel"), onPressed: () => Navigator.pop(context, 'Cancel')),
+                TextButton(child: const Text('Adicionar'), 
+                onPressed: () {
+
+               
+                 
+                    CreateClient( _controllerName.text, 
+                  double.tryParse(_controllerDivida.text) ?? 0,
+                   _controllerEndereco.text);
+
+               
+                       Navigator.pop(context, 'Ok');
+
+                
+                
+  }),
+              ]
+            )
+          ); },
+          icon:  Icon(Icons.add),
+          label: Text("")
+         )
+          
+         
+        ],
+      ),
       body: LayoutBuilder(
 
         builder: (context, constraints) {
@@ -38,18 +209,55 @@ class _HomePageState extends State<HomePage> {
               padding: const EdgeInsets.all(8.0),
               child: Column(
                 children: [
-                  TextField(
-                    decoration: InputDecoration(
-                      suffixIcon: Icon(Icons.search),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10)
-                      )
+                  SizedBox(height: 10,),
+                  Container(
+                    width: constraints.maxWidth * 0.9,
+          
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: const Color.fromARGB(255, 142, 206, 211)
+                    ),
+                    child: TextField(
+                      onChanged: _search,
+                      
+                      decoration: InputDecoration(
+                        
+                        prefixIcon: Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 15),
+                          child: Icon(Icons.search, color: Colors.white, size: 30),
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: BorderSide(
+                            color: Colors.red
+                          )
+                        )
+                      ),
                     ),
                   ),
-                  Text("Hello wordo"),
-                  Client(),
-                ],
-              ),
+
+                  SizedBox(height: 30,),
+
+                  
+
+                  SizedBox(
+                    height: 320,
+                    width: constraints.maxWidth * 0.9,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: listClient.length,
+                      itemBuilder: (context, index){
+                        final client = listClient[index];
+                        return Client(
+                          name: client['name'],
+                          divida: client['divida'],
+                          endereco: client['endereco'],
+                          key: ValueKey(client['name']),
+                        );
+                      },
+                                    ),
+                  ),
+        ])
             ),
           ),
         );
